374bba1fabef11d464c324dd0f3bb1f690ca2319
Jimmy Chang <jimmyfchang94@gmail.com>
2018-06-05 12:07:35 -0700

    completes the challenge array binary search

---
+++ Challenges/BinarySearch/BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] array = new int[10];
+            // int[] array = new int[100];
+            // int[] array = new int[1000];
+            // int[] array = new int[10000];
+            int[] array = new int[100000];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = i;
+            int key = -10;
+            int result = BinarySearch(array, key);
+            Console.WriteLine(result);
+        }
+
+        static int BinarySearch(int[] array, int key)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+            while (start <= end)
+            {
+                int mid = (start + end) / 2;
+                Console.WriteLine(mid);
+                if (array[mid] < key)
+                    start = mid + 1;
+                else if (array[mid] > key)
+                    end = mid - 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+}
---
+++ Challenges/BinarySearch/README.md
@@ -0,0 +1,17 @@
+# Binary Search
+
+The idea behind this challenge is to find a specific value in a sorted array. This is to let people understand the code behind the built-in methods that they might take for granted.
+
+# Challenge:
+
+Write a function called BinarySearch which takes in 2 parameters: a sorted array and the search key. Without utilizing any of the built-in methods available to your language, return the index of the array’s element that is equal to the search key, or -1 if the element does not exist.
+
+# Solution:
+
+Here is an image of the whiteboard activity that was done for this challenge.
+
+![Binary Search](../../assets/array_binary_search.jpg)
+
+# Explanation:
+
+We solved the challenge in a recursive method because it is the most time efficient method (with regards to the Big O) to solve this problem.
