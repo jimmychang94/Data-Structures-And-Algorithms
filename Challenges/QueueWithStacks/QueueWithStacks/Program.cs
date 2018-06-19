d59c0e3151f93f56174db9d050283f07ab43a8c1
Jimmy Chang <jimmyfchang94@gmail.com>
2018-06-18 19:20:36 -0700

    finishes the challenge, adds the readme and the whiteboard image

---
+++ Challenges/QueueWithStacks/QueueWithStacks/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using QueueWithStacks.Classes;
+
+namespace QueueWithStacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            QueueWithStacks();
+        }
+
+        /// <summary>
+        /// This method tests the enqueueing and dequeueing with stacks
+        /// </summary>
+        static void QueueWithStacks()
+        {
+            Stack stack1 = new Stack(new Node(1));
+            Stack stack2 = new Stack(new Node(1));
+
+            Queue queue = new Queue(stack1, stack2);
+
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            Console.WriteLine(queue.Stack1.Top.Value);
+
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Dequeue().Value);
+            Console.WriteLine(queue.Stack1.Top.Value);
+
+        }
+    }
+}
---
+++ Challenges/QueueWithStacks/README.md
@@ -0,0 +1,19 @@
+# Implement a Queue using two Stacks
+We recently learned about stacks and queues;  
+Stacks follow the idea of First In Last Out (FILO) or Last In First Out (LIFO).  
+On the other hand, Queues follow the idea of First In First Out (FIFO).  
+The idea for this challenge is to use two stacks to copy a queue format (FIFO).  
+
+## Challenge
+Implement the following methods for the Queue class:  
+
+	Enqueue(value) which inserts value into the Queue using a first-in, first-out approach.
+	Dequeue() which extracts a value from the Queue using a first-in, first-out approach.
+You have access to 2 Stack instances with push and pop methods.
+
+## Solution
+![Whiteboard of Initial Idea For Queue With 2 Stacks](../../assets/queue_with_stacks.jpg)
+
+## Explanation
+While writing this whiteboard we weren't sure about what the problem was asking so in the end our pseudocode was somewhat rushed.  
+Still, the basics of the pseudocode was correct and was implemented in my actual code.  
